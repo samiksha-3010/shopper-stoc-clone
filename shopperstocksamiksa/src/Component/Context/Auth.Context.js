5f4c9fa5f2c2642733e6d1a57fce39e24390f0e1
samiksha-3010 <samiksha30102@gmail.com>
2023-09-05 14:25:27 +0530

    updates

--- shopperstocksamiksa/src/Component/Context/Auth.Context.js
+++ shopperstocksamiksa/src/Component/Context/Auth.Context.js
@@ -1,50 +1,50 @@
-
 import axios from "axios";
-import { createContext, useEffect, useReducer } from "react";
+import React,{ createContext, useEffect, useReducer } from "react";
 
 export const AuthContext = createContext();
 
 const initialState = { user: null };
 
 const reducer = (state, action) => {
-    switch (action.type) {
-        case 'LOGIN':
-            return { ...state, user: action.payload }
-        case 'LOGOUT':
-            return { ...state, user: null }
-        default:
-            return state
+  switch (action.type) {
+    case "LOGIN":
+      return { ...state, user: action.payload };
+    case "LOGOUT":
+      return { ...state, user: null };
+    default:
+      return state;
+  }
+};
+
+const AuthProvider = ({ children }) => {
+  const [state, dispatch] = useReducer(reducer, initialState);
+
+  useEffect(() => {
+    async function getCurrentUserData() {
+      var token = JSON.parse(localStorage.getItem("token"));
+      const response = await axios.post(
+        "http://localhost:8000/get-current-user",
+        { token }
+      );
+      if (response.data.success) {
+        dispatch({
+          type: "LOGIN",
+          payload: response.data.user,
+        });
+      } else {
+        dispatch({
+          type: "LOGOUT",
+        });
+      }
     }
-}
+    getCurrentUserData();
+  }, []);
 
+  return (
+    <AuthContext.Provider value = {{ state, dispatch }}>
+      {{ children }}
+    </AuthContext.Provider>
+  );
+};
 
-  export const AuthProvider = ({ children }) => {
-    const [state, dispatch] = useReducer(reducer, initialState)
-
-    useEffect(() => {
-        async function getCurrentUserData() {
-            var token = JSON.parse(localStorage.getItem("token"));
-            const response = await axios.post("http://localhost:8000/get-current-user", { token });
-            if (response.data.success) {
-                dispatch({
-                    type: "LOGIN",
-                    payload: response.data.user
-                })
-            } else {
-                dispatch({
-                    type: "LOGOUT"
-                });
-            }
-        }
-        getCurrentUserData();
-    }, [])
-
-    return (
-        <AuthContext.Provider value={{ state, dispatch }}>
-            {children}
-        </AuthContext.Provider>
-    )
-
-}
-
-export default AuthContext;
+export default AuthProvider;
--- shopperstocksamiksa/src/index.js
+++ shopperstocksamiksa/src/index.js
@@ -1,47 +1,19 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
-import { BrowserRouter } from 'react-router-dom';
-import{ Toaster } from'react-hot-toast'
-import AuthProvider from './Component/Context/Auth.Context';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
+import { BrowserRouter } from "react-router-dom";
+import {AuthContext} from "./Component/Context/Auth.Context";
 
-
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-       <AuthProvider>
-    <BrowserRouter> 
-
-    <Toaster
-        position="top-center"
-        reverseOrder={false}
-        gutter={8}
-        containerClassName=""
-        containerStyle={{}}
-        toastOptions={{
-          // Define default options
-          className: '',
-          duration: 5000,
-          style: {
-            background: '#363636',
-            color: '#fff',
-          },
-
-          // Default options for specific types
-          success: {
-            duration: 3000,
-            theme: {
-              primary: 'green',
-              secondary: 'black',
-            },
-          },
-        }}
-      />
-    <App />
-    </BrowserRouter>
-    </AuthProvider>
+    <AuthContext>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </AuthContext>
   </React.StrictMode>
 );
 
