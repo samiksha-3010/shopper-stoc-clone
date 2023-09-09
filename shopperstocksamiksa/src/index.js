3b490a8e42ba1ce44fa99ccb9eb156c81186fa7b
samiksha-3010 <samiksha30102@gmail.com>
2023-09-09 17:00:06 +0530

    updates

--- shopperstocksamiksa/src/index.js
+++ shopperstocksamiksa/src/index.js
@@ -5,6 +5,8 @@ import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 import { BrowserRouter } from "react-router-dom";
 import {AuthContext} from "./Component/Context/Auth.Context";
+import { Toaster } from "react-hot-toast";
+
 
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
@@ -13,6 +15,32 @@ root.render(
     <AuthContext>
       <BrowserRouter>
         <App />
+        <Toaster
+  position="top-center"
+  reverseOrder={false}
+  gutter={8}
+  containerClassName=""
+  containerStyle={{}}
+  toastOptions={{
+    // Define default options
+    className: '',
+    duration: 5000,
+    style: {
+      background: '#363636',
+      color: '#fff',
+    },
+
+    // Default options for specific types
+    success: {
+      duration: 3000,
+      theme: {
+        primary: 'green',
+        secondary: 'black',
+      },
+    },
+  }}
+/>
+
       </BrowserRouter>
     </AuthContext>
   </React.StrictMode>
