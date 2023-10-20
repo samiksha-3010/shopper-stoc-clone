69c900fedaa90f15c3558101e6952ecaac3a155c
samiksha-3010 <samiksha30102@gmail.com>
2023-10-20 15:40:21 +0530

    done

--- shopperstocksamiksa/backendClone/index.js
+++ shopperstocksamiksa/backendClone/index.js
@@ -3,6 +3,8 @@ import dotenv from 'dotenv'
 import mongoose from 'mongoose';
 import cors from "cors"
 import  { Login, Register, getCurrentUser } from './Controolers/User.Controolers.js';
+import { checkSeller, isAdmin } from './Meedleware/All.Meedleware.js';
+import { removeCartProduct } from './Controolers/Buyer.controller.js';
 
 
 const app = express();
@@ -17,6 +19,50 @@ app.get("/", (req, res) => {
 app.post("/register", Register)
 app.post("/login", Login)
 app.post("/get-current-user",getCurrentUser )
+
+app.post("/add-product", checkSeller, addProduct);
+
+
+app.get("/all-products", allProduct);
+
+// app.post("/get-number", getNumber)
+// app.post("/send-otp", sendOtp)
+// app.post("/verify-otp", verifyOtp)
+
+app.post("/get-your-products", checkSeller, getYourProducts);
+app.post("/update-your-product", checkSeller, updateYourProduct);
+app.post("/add-cart", addToCart);
+
+app.post("/get-single-product-data", getSingleProductData);
+
+app.post("/add-to-cart", addCart);
+
+app.post("/remove-cart-items", removeCartProduct );
+
+app.post("/all-cart-products", allCartProducts);
+
+app.get("/get-cart-products", getCartProducts);
+
+// app.post("/checkout", checkOut);
+app.post("/delete-your-product",checkSeller, deleteYourProduct);
+
+app.patch("/block-user", isAdmin, blockUser);
+
+app.patch("/un-block-user", isAdmin, unBlockUser);
+
+app.patch("/block-product", isAdmin, blockProduct);
+
+app.patch("/un-block-product", isAdmin, unBlockProduct );
+
+app.patch("/verify-product", isAdmin, verifyProduct);
+
+
+app.get("/get-all-buyers", isAdmin, getAllBuyers ); 
+app.get("/get-all-sellers", isAdmin, getAllSellers); 
+app.get("/get-all-products", isAdmin, getAllProducts); 
+
+app.patch("/get-verify-product", isAdmin, getVerifiedProducts); 
+app.patch("/get-un-verify-product", isAdmin, getUnVerifiedProducts); 
 
 
 
